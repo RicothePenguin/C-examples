0e87ec8873e1372a2a50c16c372bdfa2588681eb
Semih Arda <semiharda2000@outlook.com>
2021-07-26 12:35:29 +0300

    Multiply.c modified

--- Multiply.c
+++ Multiply.c
@@ -1,1 +1,12 @@
  #include <stdio.h>
+int multiply(int a,int b) {
+    int sum;
+    sum = a * b;
+    return sum;
+}
+ int main() {
+    int x,y;
+    scanf("%d %d",&x,&y);
+    printf("Multiply of entered numbers:%d\n",multiply(x,y));
+    return 0;
+}
